057d64ccfda5f9d6044d98bc8a670c9e888e3363
JAS0N-SMITH <huchknows@gmail.com>
2025-01-21 12:15:20 -0500

    Init: Angular project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CalculatorBasicComponent } from './components/calculator-basic/calculator-basic.component';
+import { CalculatorScientificComponent } from './components/calculator-scientific/calculator-scientific.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'basic', component: CalculatorBasicComponent },
+  { path: 'scientific', component: CalculatorScientificComponent },
+  { path: '', redirectTo: 'basic', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: 'basic' }, // Wildcard route
+];
