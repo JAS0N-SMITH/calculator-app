8f81ddc5f40e296142a80595de8c09e325e93503
JAS0N-SMITH <huchknows@gmail.com>
2025-01-21 13:07:48 -0500

    feat: add calculator service and tests

--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { evaluate } from 'mathjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculatorService {
-
-  constructor() { }
+  calculate(expression: string): number {
+    try {
+      const result = evaluate(expression);
+      if (isNaN(result)) {
+        throw new Error('Invalid equation');
+      }
+      return result;
+    } catch (error) {
+      throw new Error('Invalid equation');
+    }
+  }
 }
