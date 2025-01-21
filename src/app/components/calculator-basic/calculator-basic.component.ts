55bd6857c9b5a1c2d20db4248047a62261f63ffb
JAS0N-SMITH <huchknows@gmail.com>
2025-01-21 14:27:53 -0500

    feat: added styling for calculator component; error prevention/handling;

--- src/app/components/calculator-basic/calculator-basic.component.ts
+++ src/app/components/calculator-basic/calculator-basic.component.ts
@@ -1,3 +1,4 @@
+// calculator-basic.component.ts
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -8,7 +9,7 @@ import { CalculatorService } from '../../services/calculator.service';
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './calculator-basic.component.html',
-  styleUrl: './calculator-basic.component.scss',
+  styleUrls: ['./calculator-basic.component.scss'],
 })
 export class CalculatorBasicComponent {
   input = '';
@@ -16,22 +17,30 @@ export class CalculatorBasicComponent {
     '7',
     '8',
     '9',
+    '+',
     '4',
     '5',
     '6',
+    '-',
     '1',
     '2',
     '3',
-    '0',
-    '+',
-    '-',
     '*',
+    '0',
     '/',
   ];
 
   constructor(private calculatorService: CalculatorService) {}
 
   append(value: string) {
+    // Prevent consecutive operators
+    const lastChar = this.input.slice(-1);
+    if (
+      ['+', '-', '*', '/'].includes(lastChar) &&
+      ['+', '-', '*', '/'].includes(value)
+    ) {
+      return;
+    }
     this.input += value;
   }
 
@@ -41,9 +50,11 @@ export class CalculatorBasicComponent {
 
   calculate() {
     try {
+      // Use CalculatorService to evaluate the expression
       this.input = this.calculatorService.calculate(this.input).toString();
     } catch (error) {
       alert('Invalid expression');
+      this.clear();
     }
   }
 }
