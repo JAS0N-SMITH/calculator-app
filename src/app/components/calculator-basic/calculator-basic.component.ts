e101854337c367cb310f8171284e64732f9ec663
JAS0N-SMITH <huchknows@gmail.com>
2025-01-21 13:20:21 -0500

    feat: added basic calculator input and buttons to component

--- src/app/components/calculator-basic/calculator-basic.component.ts
+++ src/app/components/calculator-basic/calculator-basic.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CalculatorService } from '../../services/calculator.service';
 
 @Component({
   selector: 'app-calculator-basic',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculator-basic.component.html',
-  styleUrl: './calculator-basic.component.scss'
+  styleUrl: './calculator-basic.component.scss',
 })
 export class CalculatorBasicComponent {
+  input = '';
+  buttons = [
+    '7',
+    '8',
+    '9',
+    '4',
+    '5',
+    '6',
+    '1',
+    '2',
+    '3',
+    '0',
+    '+',
+    '-',
+    '*',
+    '/',
+  ];
 
+  constructor(private calculatorService: CalculatorService) {}
+
+  append(value: string) {
+    this.input += value;
+  }
+
+  clear() {
+    this.input = '';
+  }
+
+  calculate() {
+    try {
+      this.input = this.calculatorService.calculate(this.input).toString();
+    } catch (error) {
+      alert('Invalid expression');
+    }
+  }
 }
