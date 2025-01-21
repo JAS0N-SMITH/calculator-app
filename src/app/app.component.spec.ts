e5da232ee3e165c8c77a120a627213e6f3d78edd
JAS0N-SMITH <huchknows@gmail.com>
2025-01-21 13:54:37 -0500

    feat(test): added test for dark mode button

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,16 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { ThemeService } from './shared/theme.service';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let themeService: jasmine.SpyObj<ThemeService>;
 
   beforeEach(async () => {
+    const themeServiceSpy = jasmine.createSpyObj('ThemeService', [
+      'toggleTheme',
+    ]);
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [{ provide: ActivatedRoute, useValue: { params: of({}) } }],
+      providers: [
+        { provide: ActivatedRoute, useValue: { params: of({}) } },
+        { provide: ThemeService, useValue: themeServiceSpy },
+      ],
     }).compileComponents();
+
+    themeService = TestBed.inject(ThemeService) as jasmine.SpyObj<ThemeService>;
   });
 
   beforeEach(() => {
@@ -25,5 +35,16 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1').textContent).toContain(
       'calculator-app'
     );
+  });
+
+  it('should call toggleTheme on themeService', () => {
+    component.toggleTheme();
+    expect(themeService.toggleTheme).toHaveBeenCalled();
+  });
+
+  it('should toggle theme when button is clicked', () => {
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    expect(themeService.toggleTheme).toHaveBeenCalled();
   });
 });
